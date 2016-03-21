cfab753345f89f43873d059eee0f00e8479f2831
Cameron Norman <cameronrossnorman@gmail.com>
2016-03-21 19:21:26 +0200

    added cool plugins

---
+++ vim_package_install.sh
@@ -0,0 +1,9 @@
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/scrooloose/nerdcommenter
+git clone https://github.com/dsawardekar/ember.vim
+git clone https://github.com/bronson/vim-trailing-whitespace
